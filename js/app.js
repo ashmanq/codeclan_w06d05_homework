6028b150c7d0fc4e4ea4adaec6c10f57d26862af
Ashir <ashirq@outlook.com>
2020-04-17 16:17:56 +0100

    Refactored JavaScript code to be more easily readable and to seperate out code.

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   const buttonClick = document.querySelector('#deleteAllBtn');
   buttonClick.addEventListener('click', handleButtonClick);
-  
+
   const formInput = document.querySelector('#formSubmit');
   formInput.addEventListener('submit', handleFormSubmission);
 
@@ -16,35 +16,39 @@ const handleButtonClick = (event) => {
 
 const handleFormSubmission = (event) => {
   event.preventDefault();
+
+  const cartoonItem = createCartoonListItem(event.target);
+  // We add the new cartoonItem to the cartoon list
   const listNode = document.querySelector('#cartoonList');
+  listNode.appendChild(cartoonItem);
+
+  // Form reset after entries are added to the list
+  event.target.reset();
+
+}
+
+const createCartoonListItem = (form) => {
 
   // First we create a section that will hold the cartoon details
-
   const sectionElement = document.createElement('section');
+  // Allocate list to class for css purposes
   sectionElement.className = 'list';
 
-  // Then we create the three paragraphs that will hold the title
-  // description and year information
-
+  // Then we create the three paragraphs that will hold the title,
+  // description and year information then add them to the section
+  // element
   const titleParagraph = document.createElement('h2');
-  titleParagraph.innerText = event.target.title.value;
-  const descriptionParagraph = document.createElement('h3');
-  descriptionParagraph.innerText = event.target.desc.value;
-  const yearParagraph = document.createElement('p');
-  yearParagraph.innerText = event.target.year.value;
-
+  titleParagraph.innerText = form.title.value;
   sectionElement.appendChild(titleParagraph);
+
+  const descriptionParagraph = document.createElement('h3');
+  descriptionParagraph.innerText = form.desc.value;
   sectionElement.appendChild(descriptionParagraph);
+
+  const yearParagraph = document.createElement('p');
+  yearParagraph.innerText = form.year.value;
   sectionElement.appendChild(yearParagraph);
 
-  listNode.appendChild(sectionElement);
-
-  event.target.title.value = '';
-  event.target.desc.value = '';
-  event.target.year.value = '';
-
-  // We then add the paragraphs to the section elements
-
-  // We finally add the section element to the cartoons list
+  return sectionElement;
 
 }
